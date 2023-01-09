301ee6d067ec9a5547762a14978f47ad2fe1722d
sebastientromp <sebastien.tromp@gmail.com>
2023-01-09 21:43:41 +0100

    initial commit

---
+++ NoMercsDiamondVoiceLinesMod.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader;
+using UnityEngine;
+using HarmonyLib;
+using System.Reflection;
+using System;
+
+namespace NoMercsDiamondVoiceLines
+{
+    public class NoMercsDiamondVoiceLinesMod : MelonMod
+    {
+        public static MelonLogger.Instance SharedLogger;
+
+        public override void OnInitializeMelon()
+        {
+            NoMercsDiamondVoiceLinesMod.SharedLogger = LoggerInstance;
+            var harmony = this.HarmonyInstance;
+            harmony.PatchAll(typeof(PlayEmotePatcher));
+        }
+    }
+
+    public static class PlayEmotePatcher
+    {
+        [HarmonyPatch(typeof(Card), "PlayEmote", new Type[] { typeof(EmoteType), typeof(Notification.SpeechBubbleDirection) })]
+        [HarmonyPrefix]
+        public static bool Prefix(EmoteType emoteType, Notification.SpeechBubbleDirection overrideDirection, ref CardSoundSpell __result, Card __instance)
+        {
+            if (__instance.GetEntity().IsLettuceMercenary())
+            {
+                NoMercsDiamondVoiceLinesMod.SharedLogger.Msg($"Blocking Mercenary Emote, emoteType={emoteType}, direction={overrideDirection}");
+                __result = null;
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Just drop dist/AutoSquelch.dll into your Hearthstone Mods folder (you need to have installed MelonLoader first)
